c7f173b28e7c5d49210d161576f478228af0144e
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-17 12:55:00 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1-mashq
+s = {1, 2, 3}
+print(s)
+# 2-mashq
+s = {1, 2}
+s.add(3)
+print(s)
+# 3-mashq
+s = {1, 2, 3}
+s.remove(2)
+print(s)
+# 4-mashq
+a = {1, 2}
+b = {2, 3}
+print(a | b)
+# 5-mashq
+print(a & b)
